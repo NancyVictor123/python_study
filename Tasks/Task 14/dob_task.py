461c3431ef9dad40ca4c0bf372108f41438bc2f9
NancyVictor <nvictor200690@gmail.com>
2024-01-29 14:01:11 +0000

    - DOB for loop optimization

--- Tasks/Task 14/dob_task.py
+++ Tasks/Task 14/dob_task.py
@@ -4,20 +4,21 @@ with open(file_path, "r") as data_file:
     dob_data = data_file.readlines()
 
 # Declare variable to Store names and date of birth
-names = []
+#names = []
 birthday = []
 
+print("Name: ")
 for line in dob_data:
     splitted_data = line.split()
     name = ' '.join(splitted_data[:2])
     dob = ' '.join(splitted_data[-3:])
-    names.append(name)
+    #names.append(name)
+    print(name)
     birthday.append(dob)
 
 # Print names and dates of birth separately
-print("Name: ")
-for name in names:
-    print(name)
+# for name in names:
+#     print(name)
 print("\nBirthday: ")
 for dob in birthday:
     print(dob)
