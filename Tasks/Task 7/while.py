084d39e4d3f681f7e5fab3c5b8ad9befb1ec394a
Nancy Victor <nvictor200690@gmail.com>
2024-03-28 11:13:13 +0000

    changed attempt to trial

--- Tasks/Task 7/while.py
+++ Tasks/Task 7/while.py
@@ -1,21 +1,21 @@
 """
 Start of the pseudo code
 Declare a variable sum to add the numbers entered by user
-When user_input not equal to -1 the attempt should increment 
+When user_input not equal to -1 the trial should increment 
 and the number should be added and prompt the user to add another number
 When the user enters -1, the loop should stop 
 and the average of numbers entered by user should be printed
 end of the pseudo code
 """
 user_input = 0
-attempt = 0
+trial = 0
 sum = 0
 average = 0 
 while user_input != -1:
     user_input = float(input("Please enter a number : "))
     if user_input != -1:
-        attempt += 1
+        trial += 1
         sum +=  user_input
     else:    
-        average = sum / attempt
+        average = sum / trial
         print(f"The average of numbers you entered is {average:.2f}")
